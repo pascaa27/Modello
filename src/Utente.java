affbb9e4f94c07ccfa41d27749ac8fcc7e3087aa
frapi <francesco.pisano5@studenti.unina.it>
2025-04-18 18:02:22 +0200

    Classe Amministratore modificata.

--- src/Utente.java
+++ src/Utente.java
@@ -4,27 +4,29 @@ public class Utente {
     public String password;
 
     public Utente(String login, String password) {
-
         this.login = login;
         this.password = password;
-
     }
 
     public String getLogin() {
-
         return login;
     }
 
     public String getPassword() {
-
         return password;
     }
 
-    public void modificaPrenotazione(){
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void modificaPrenotazione(){
     }
 
     public void visualizzaAggiornamentoVolo(){
-
     }
 }
